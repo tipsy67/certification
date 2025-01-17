8608c5c3822de5ae5b347bd97612428e9902090f
dan.toropov <dan.toropov@gmail.com>
2025-01-17 22:53:36 +0300

    cretae models

--- retail/admin.py
+++ retail/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from retail.models import Contact, Product, Member
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    pass
+
--- retail/models.py
+++ retail/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+MEMBER_TYPE = (
+    ('PLNT', 'plant'),
+    ('INDV', 'individual'),
+    ('RTL', 'retail')
+)
+
+class Contact(models.Model):
+    email = models.EmailField(verbose_name='эл.почта')
+    country = models.CharField(max_length=100, verbose_name='страна')
+    city = models.CharField(max_length=100, verbose_name='город')
+    street = models.CharField(max_length=100, verbose_name='улица')
+    building = models.CharField(max_length=100, verbose_name='строение')
+    created_at = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='дата изменения')
+    member = models.ForeignKey(to='Member', on_delete=models.CASCADE, related_name='contacts', verbose_name='')
+
+    class Meta:
+        verbose_name = 'контакт'
+        verbose_name_plural = 'контакты'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.email}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    product_model = models.CharField(max_length=100, verbose_name='модель')
+    launch_date = models.DateField(verbose_name='дата выхода на рынок')
+    created_at = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='дата изменения')
+    member = models.ForeignKey(to='Member', on_delete=models.CASCADE, related_name='products', verbose_name='')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name} {self.product_model}"
+
+class Member(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    member_type = models.CharField(max_length=4, choices=MEMBER_TYPE, verbose_name='тип звена')
+    accounts_payable = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='кредиторка')
+    created_at = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='дата изменения')
+    supplier = models.OneToOneField(to='Member', on_delete=models.PROTECT, verbose_name='поставщик')
+
+    class Meta:
+        verbose_name = 'элемент сети'
+        verbose_name_plural = 'элементы сети'
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name} {self.member_type}"
