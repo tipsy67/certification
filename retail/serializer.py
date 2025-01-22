0f54f74cfab7d9d53ab29d7830246ace87ecc33e
dan.toropov <dan.toropov@gmail.com>
2025-01-23 02:06:51 +0300

    create serializers

--- retail/serializer.py
+++ retail/serializer.py
@@ -8,15 +8,42 @@ class ContactSerializer(serializers.ModelSerializer):
            model = Contact
            fields = '__all__'
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        a=1
-
 
 class MemberSerializer(serializers.ModelSerializer):
     contacts = ContactSerializer(many=True,)
 
     class Meta:
         model = Member
-        fields = ('name',  'display_member_type', 'member_level', 'accounts_payable', 'supplier', 'contacts')
+        fields = ('pk', 'name',  'display_member_type', 'member_level', 'accounts_payable', 'supplier', 'contacts')
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['contacts'] = data['contacts'][:1]
+        return data
+
+    def update(self, instance, validated_data):
+        if self.is_valid():
+            pass
+        rel_obj_validated_data = validated_data.pop('contacts', [])
+        rel_validate_data = next(iter(rel_obj_validated_data or []), None)
+
+        rel_obj_data = self.initial_data.get('contacts')
+        rel_data = next(iter(rel_obj_data or []), None)
+
+        if rel_validate_data is not None:
+            related_obj, created = Contact.objects.get_or_create(
+                    pk=rel_data.get('id', None),
+                    defaults=rel_validate_data
+                )
+            if not created:
+                for key, value in rel_validate_data.items():
+                    setattr(related_obj, key, value)
+                related_obj.save()
+
+
+
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+
+        return instance
--- retail/views.py
+++ retail/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import path, include
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from retail.models import Member, Contact
 from retail.serializer import MemberSerializer, ContactSerializer
@@ -14,6 +15,11 @@ class ContactViewSet(viewsets.ModelViewSet):
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = Member.objects.prefetch_related('contacts')
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 
